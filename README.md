fc29dcf475ee81bbc6de00dacf03825b33eca875
DutraGames <gabrieldutraamarante@gmail.com>
2021-09-08 00:00:38 -0300

    sitema de trasação e LocalStorage

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Sistema Financeiro
+
+## Passos:
+
+- [x] Layout do projeto
+- [x] Funcionalidade de transação
+- [x] Salvar no localStorage
+- [ ] Criar últimas trasações no layout
--- src/App.js
+++ src/App.js
@@ -3,23 +3,59 @@ import './App.css'
 
 export default function App() {
 
-  
 const [saldo, setSaldo] = useState(0)
 const [receita, setReceita] = useState(0)
 const [despesas, setDespesas] = useState(0)
 const [action, setAction] = useState('Salário')
 const [valor, setValor] = useState(1)
 
+useEffect(() => {
+
+  if ("saldo" in localStorage) {
+    setSaldo(parseFloat(localStorage.getItem("saldo")))
+} else {
+  localStorage.setItem('saldo', saldo)
+}
+
+if ("receita" in localStorage) {
+  setReceita(parseFloat(localStorage.getItem("receita")))
+} else {
+localStorage.setItem('receita', receita)
+}
+
+if ("despesas" in localStorage) {
+  setDespesas(parseFloat(localStorage.getItem("despesas")))
+} else {
+localStorage.setItem('despesas', despesas)
+}
+}, [])
+
 
 function AddTras(){
-  alert("Hello Word")
+  if(action === 'Salário'){
+    setReceita(receita + valor)
+  setSaldo(saldo + valor)
+
+  localStorage.setItem("saldo", parseFloat(saldo + valor))
+  localStorage.setItem("despesas", parseFloat(despesas))
+  localStorage.setItem("receita", parseFloat(receita + valor))
+
+  }else{
+    setDespesas(despesas + valor)
+    setSaldo(saldo - valor)
+
+    localStorage.setItem("saldo", parseFloat(saldo - valor))
+    localStorage.setItem("despesas", parseFloat(despesas + valor))
+    localStorage.setItem("receita", parseFloat(receita))
+  }
+
 }
 
 return(
 <div>
 
   <div className="box-saldo">
-    <h2>Controle de Despesas</h2>
+    <h2>Controle de Finanças</h2>
     <p>Saldo Atual:</p>
     <span>R$ {saldo.toFixed(2)}</span>
   </div>
@@ -41,11 +77,12 @@ return(
     <h2>Adicionar Transação</h2>
     
     <label>Transação:</label>
-    <select name="transacao">
+    <select name="transacao" onClick={(e)=> setAction(e.target.value)}>
     <option value="Salário">Salário</option>
       <option value="Conta de Luz">Conta de Luz</option>
       <option value="Conta d' Água">Conta d' Água</option>
       <option value="Internet">Internet</option>
+      <option value="Telefone">Telefone</option>
       <option value="Plano de Saúde">Plano de Saúde</option>
       <option value="Plano Funerário">Plano Funerário</option>
       <option value="Gás">Gás</option>
@@ -53,7 +90,7 @@ return(
     </select>
 
     <label>Valor:</label>
-    <input type="number" value={valor}min="1"/>
+    <input type="number" value={valor} min="1" onChange={(e)=> setValor(parseFloat(e.target.value))}/>
 
     <a className="btn" onClick={AddTras}>Adicionar</a>
   </div>
